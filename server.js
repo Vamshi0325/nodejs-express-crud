329077e6f79d018858a53d7bb309a374abba1baf
Vamshi <vamshivade2000@gmail.com>
2025-08-22 17:06:12 +0530

    first commit

--- server.js
+++ server.js
@@ -31,7 +31,7 @@ const PORT = process.env.PORT || 5000;
 
 const server = app.listen(
   PORT,
-  console.log(`Server running in ${process.env.NODE_ENV} mode on port ${PORT}`)
+  console.log(`Server running ${process.env.NODE_ENV} mode on port ${PORT}`)
 );
 
 // Handle unhandled promise rejections
